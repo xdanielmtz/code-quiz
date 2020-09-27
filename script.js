b0cab9b783602227355cd4114ebb26a76c34cc67
Dani JM <xdanielmtz@gmail.com>
2020-09-27 19:13:18 -0400

    aligned start button. added first question as well as corresponding buttons.

--- script.js
+++ script.js
@@ -8,5 +8,17 @@ var scoreCounter = document.getElementById("score");
 start.addEventListener("click", function(){
     welcomeDiv.style.display="none";
     scoreCounter.textContent="Score: " + 60;
+    questionOne();
 })
 
+function questionOne(){
+    questions.textContent = "What tag is required in all HTML documents, and is used to define the title?";
+    var answersOne = ["Title", "Body", "Head", "Br"];
+    for (var i = 0; i < answersOne.length; i++){
+        var buttonsOne = document.createElement("button");
+        buttonsOne.textContent = answersOne[i];
+        buttonsOne.setAttribute("data-value", answersOne[i]);
+        answers.append(buttonsOne);
+
+    }
+}
