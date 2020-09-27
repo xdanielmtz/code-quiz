8e06cb62ab94e3b0169325ff6685e7f7ea6d84ab
Dani JM <xdanielmtz@gmail.com>
2020-09-27 17:18:21 -0400

    added starting score of 60 when clicking start button

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var welcomeDiv = document.getElementById("welcome");
 var start = document.getElementById("startButton")
-
+var questions = document.getElementById("codeQuestions");
+var answers = document.getElementById("codeAnswers");
+var scoreCounter = document.getElementById("score");  
 
 
 start.addEventListener("click", function(){
     welcomeDiv.style.display="none";
+    scoreCounter.textContent="Score: " + 60;
 })
+
