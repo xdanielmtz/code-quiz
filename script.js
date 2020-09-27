646a07c0b9b6c3704b0c9a9ceb8ef10e308b568a
Dani JM <xdanielmtz@gmail.com>
2020-09-27 16:31:31 -0400

    added an evenet listener to disappeaer elements when clicking on start

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 var welcomeDiv = document.getElementById("welcome");
+var start = document.getElementById("startButton")
+
+
+
+start.addEventListener("click", function(){
+    welcomeDiv.style.display="none";
+})
